a7f5c03aa865cb5c7a6ff555ca9eedc658fd22f4
Kitano <k_satoshi@portart64.com>
2026-05-21 08:40:30 +0900

    functions.php見直し(include.php)

--- functions/console_log.php
+++ functions/console_log.php
@@ -1,7 +1,9 @@
 <?php
-
-function console_log($data){
-  echo '<script>';
-  echo 'console.log('.json_encode($data).')';
-  echo '</script>';
+function console_log($data)
+{
+    if (WP_DEBUG) {
+        echo '<script>';
+        echo 'console.log(' . json_encode($data) . ')';
+        echo '</script>';
+    }
 }
